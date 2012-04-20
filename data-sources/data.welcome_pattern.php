6d4fe4136968f249e9d4e5a3a7f20a9483a2e0b1
Stephen Bau <stephen@domain7.com>
2012-04-20 11:43:02 -0700

    Create Welcome Pattern data source

---
+++ data-sources/data.welcome_pattern.php
@@ -0,0 +1,78 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+
+	Class datasourcewelcome_pattern extends Datasource{
+
+		public $dsParamROOTELEMENT = 'welcome-pattern';
+		public $dsParamORDER = 'asc';
+		public $dsParamPAGINATERESULTS = 'no';
+		public $dsParamLIMIT = '20';
+		public $dsParamSTARTPAGE = '1';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'title';
+		public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
+
+		public $dsParamFILTERS = array(
+				'9' => 'Yes',
+		);
+
+		public $dsParamINCLUDEDELEMENTS = array(
+				'title: formatted',
+				'category',
+				'date',
+				'publish',
+				'html: unformatted',
+				'css: unformatted'
+		);
+
+
+		public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array();
+		}
+
+		public function about(){
+			return array(
+				'name' => 'Welcome Pattern',
+				'author' => array(
+					'name' => 'Stephen Bau',
+					'website' => 'http://home/sym/pears',
+					'email' => 'stephen@domain7.com'),
+				'version' => 'Symphony 2.2.5',
+				'release-date' => '2012-04-20T18:37:27+00:00'
+			);
+		}
+
+		public function getSource(){
+			return '2';
+		}
+
+		public function allowEditorToParse(){
+			return true;
+		}
+
+		public function grab(&$param_pool=NULL){
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+
+			try{
+				include(TOOLKIT . '/data-sources/datasource.section.php');
+			}
+			catch(FrontendPageNotFoundException $e){
+				// Work around. This ensures the 404 page is displayed and
+				// is not picked up by the default catch() statement below
+				FrontendPageNotFoundExceptionHandler::render($e);
+			}
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage()));
+				return $result;
+			}
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			
+
+			return $result;
+		}
+
+	}
