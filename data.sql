edebd9218292ddca745734d5a29daa8ced7b0e92
Stephen Bau <stephen@domain7.com>
2012-04-20 00:12:05 -0700

    Save data.sql file from Dump DB extension

---
+++ data.sql
@@ -0,0 +1,295 @@
+
+-- *** STRUCTURE: `sym_cache` ***
+DROP TABLE IF EXISTS `sym_cache`;
+CREATE TABLE `sym_cache` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `hash` varchar(32) NOT NULL DEFAULT '',
+  `creation` int(14) NOT NULL DEFAULT '0',
+  `expiry` int(14) unsigned DEFAULT NULL,
+  `data` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `expiry` (`expiry`),
+  KEY `hash` (`hash`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** STRUCTURE: `sym_entries` ***
+DROP TABLE IF EXISTS `sym_entries`;
+CREATE TABLE `sym_entries` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `section_id` int(11) unsigned NOT NULL,
+  `author_id` int(11) unsigned NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `creation_date_gmt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `section_id` (`section_id`),
+  KEY `author_id` (`author_id`),
+  KEY `creation_date` (`creation_date`),
+  KEY `creation_date_gmt` (`creation_date_gmt`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_entries` ***
+
+-- *** STRUCTURE: `sym_extensions` ***
+DROP TABLE IF EXISTS `sym_extensions`;
+CREATE TABLE `sym_extensions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `status` enum('enabled','disabled') NOT NULL DEFAULT 'enabled',
+  `version` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_extensions` ***
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (1, 'xssfilter', 'enabled', 1.1);
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (2, 'debugdevkit', 'enabled', '1.2.1');
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (3, 'dump_db', 'enabled', 1.09);
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (4, 'export_ensemble', 'enabled', 1.16);
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (5, 'selectbox_link_field', 'enabled', 1.22);
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (6, 'jit_image_manipulation', 'enabled', 1.14);
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (7, 'maintenance_mode', 'enabled', 1.5);
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (8, 'profiledevkit', 'enabled', '1.0.4');
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (9, 'markdown', 'enabled', 1.13);
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (10, 'edui', 'enabled', '0.6.1');
+INSERT INTO `sym_extensions` (`id`, `name`, `status`, `version`) VALUES (11, 'html5_doctype', 'enabled', '1.2.5');
+
+-- *** STRUCTURE: `sym_extensions_delegates` ***
+DROP TABLE IF EXISTS `sym_extensions_delegates`;
+CREATE TABLE `sym_extensions_delegates` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `extension_id` int(11) NOT NULL,
+  `page` varchar(100) NOT NULL,
+  `delegate` varchar(100) NOT NULL,
+  `callback` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `extension_id` (`extension_id`),
+  KEY `page` (`page`),
+  KEY `delegate` (`delegate`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_extensions_delegates` ***
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (1, 1, '/blueprints/events/new/', 'AppendEventFilter', 'appendEventFilter');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (2, 1, '/blueprints/events/edit/', 'AppendEventFilter', 'appendEventFilter');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (3, 1, '/frontend/', 'EventPreSaveFilter', 'eventPreSaveFilter');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (4, 2, '/frontend/', 'FrontendDevKitResolve', 'frontendDevKitResolve');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (5, 2, '/frontend/', 'ManipulateDevKitNavigation', 'manipulateDevKitNavigation');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (6, 3, '/system/preferences/', 'AddCustomPreferenceFieldsets', 'appendPreferences');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (7, 3, '/backend/', 'InitaliseAdminPageHead', 'initaliseAdminPageHead');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (8, 3, '/backend/', 'AppendPageAlert', 'appendAlert');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (9, 4, '/system/preferences/', 'AddCustomPreferenceFieldsets', 'appendPreferences');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (10, 6, '/system/preferences/', 'AddCustomPreferenceFieldsets', 'appendPreferences');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (11, 6, '/system/preferences/', 'Save', '__SavePreferences');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (12, 7, '/system/preferences/', 'AddCustomPreferenceFieldsets', 'appendPreferences');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (13, 7, '/system/preferences/', 'Save', '__SavePreferences');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (14, 7, '/system/preferences/', 'CustomActions', '__toggleMaintenanceMode');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (15, 7, '/backend/', 'AppendPageAlert', '__appendAlert');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (16, 7, '/blueprints/pages/', 'AppendPageContent', '__appendType');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (17, 7, '/frontend/', 'FrontendPrePageResolve', '__checkForMaintenanceMode');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (18, 7, '/frontend/', 'FrontendParamsResolve', '__addParam');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (19, 8, '/frontend/', 'FrontendDevKitResolve', 'frontendDevKitResolve');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (20, 8, '/frontend/', 'ManipulateDevKitNavigation', 'manipulateDevKitNavigation');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (21, 10, '/backend/', 'NavigationPreRender', 'deleteComponentsItem');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (22, 10, '/backend/', 'AdminPagePreGenerate', 'setRedirects');
+INSERT INTO `sym_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (23, 11, '/frontend/', 'FrontendOutputPostGenerate', 'parse_html');
+
+-- *** STRUCTURE: `sym_fields` ***
+DROP TABLE IF EXISTS `sym_fields`;
+CREATE TABLE `sym_fields` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `label` varchar(255) NOT NULL,
+  `element_name` varchar(50) NOT NULL,
+  `type` varchar(32) NOT NULL,
+  `parent_section` int(11) NOT NULL DEFAULT '0',
+  `required` enum('yes','no') NOT NULL DEFAULT 'yes',
+  `sortorder` int(11) NOT NULL DEFAULT '1',
+  `location` enum('main','sidebar') NOT NULL DEFAULT 'main',
+  `show_column` enum('yes','no') NOT NULL DEFAULT 'no',
+  PRIMARY KEY (`id`),
+  KEY `index` (`element_name`,`type`,`parent_section`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields` ***
+
+-- *** STRUCTURE: `sym_fields_author` ***
+DROP TABLE IF EXISTS `sym_fields_author`;
+CREATE TABLE `sym_fields_author` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `allow_author_change` enum('yes','no') NOT NULL,
+  `allow_multiple_selection` enum('yes','no') NOT NULL DEFAULT 'no',
+  `default_to_current_user` enum('yes','no') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `field_id` (`field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields_author` ***
+
+-- *** STRUCTURE: `sym_fields_checkbox` ***
+DROP TABLE IF EXISTS `sym_fields_checkbox`;
+CREATE TABLE `sym_fields_checkbox` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `default_state` enum('on','off') NOT NULL DEFAULT 'on',
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `field_id` (`field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields_checkbox` ***
+
+-- *** STRUCTURE: `sym_fields_date` ***
+DROP TABLE IF EXISTS `sym_fields_date`;
+CREATE TABLE `sym_fields_date` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `pre_populate` enum('yes','no') NOT NULL DEFAULT 'no',
+  PRIMARY KEY (`id`),
+  KEY `field_id` (`field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields_date` ***
+
+-- *** STRUCTURE: `sym_fields_input` ***
+DROP TABLE IF EXISTS `sym_fields_input`;
+CREATE TABLE `sym_fields_input` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `validator` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `field_id` (`field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields_input` ***
+
+-- *** STRUCTURE: `sym_fields_select` ***
+DROP TABLE IF EXISTS `sym_fields_select`;
+CREATE TABLE `sym_fields_select` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `allow_multiple_selection` enum('yes','no') NOT NULL DEFAULT 'no',
+  `show_association` enum('yes','no') NOT NULL DEFAULT 'yes',
+  `sort_options` enum('yes','no') NOT NULL DEFAULT 'no',
+  `static_options` text,
+  `dynamic_options` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `field_id` (`field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields_select` ***
+
+-- *** STRUCTURE: `sym_fields_selectbox_link` ***
+DROP TABLE IF EXISTS `sym_fields_selectbox_link`;
+CREATE TABLE `sym_fields_selectbox_link` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `allow_multiple_selection` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
+  `show_association` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'yes',
+  `related_field_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `limit` int(4) unsigned NOT NULL DEFAULT '20',
+  PRIMARY KEY (`id`),
+  KEY `field_id` (`field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- *** DATA: `sym_fields_selectbox_link` ***
+
+-- *** STRUCTURE: `sym_fields_taglist` ***
+DROP TABLE IF EXISTS `sym_fields_taglist`;
+CREATE TABLE `sym_fields_taglist` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `validator` varchar(255) DEFAULT NULL,
+  `pre_populate_source` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `field_id` (`field_id`),
+  KEY `pre_populate_source` (`pre_populate_source`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields_taglist` ***
+
+-- *** STRUCTURE: `sym_fields_textarea` ***
+DROP TABLE IF EXISTS `sym_fields_textarea`;
+CREATE TABLE `sym_fields_textarea` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `formatter` varchar(100) DEFAULT NULL,
+  `size` int(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `field_id` (`field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields_textarea` ***
+
+-- *** STRUCTURE: `sym_fields_upload` ***
+DROP TABLE IF EXISTS `sym_fields_upload`;
+CREATE TABLE `sym_fields_upload` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `destination` varchar(255) NOT NULL,
+  `validator` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `field_id` (`field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_fields_upload` ***
+
+-- *** STRUCTURE: `sym_pages` ***
+DROP TABLE IF EXISTS `sym_pages`;
+CREATE TABLE `sym_pages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `parent` int(11) DEFAULT NULL,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `handle` varchar(255) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `params` varchar(255) DEFAULT NULL,
+  `data_sources` text,
+  `events` text,
+  `sortorder` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `parent` (`parent`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_pages` ***
+
+-- *** STRUCTURE: `sym_pages_types` ***
+DROP TABLE IF EXISTS `sym_pages_types`;
+CREATE TABLE `sym_pages_types` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `page_id` int(11) unsigned NOT NULL,
+  `type` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `page_id` (`page_id`,`type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_pages_types` ***
+
+-- *** STRUCTURE: `sym_sections` ***
+DROP TABLE IF EXISTS `sym_sections`;
+CREATE TABLE `sym_sections` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `handle` varchar(255) NOT NULL,
+  `sortorder` int(11) NOT NULL DEFAULT '0',
+  `entry_order` varchar(7) DEFAULT NULL,
+  `entry_order_direction` enum('asc','desc') DEFAULT 'asc',
+  `hidden` enum('yes','no') NOT NULL DEFAULT 'no',
+  `navigation_group` varchar(255) NOT NULL DEFAULT 'Content',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `handle` (`handle`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_sections` ***
+
+-- *** STRUCTURE: `sym_sections_association` ***
+DROP TABLE IF EXISTS `sym_sections_association`;
+CREATE TABLE `sym_sections_association` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_section_id` int(11) unsigned NOT NULL,
+  `parent_section_field_id` int(11) unsigned DEFAULT NULL,
+  `child_section_id` int(11) unsigned NOT NULL,
+  `child_section_field_id` int(11) unsigned NOT NULL,
+  `hide_association` enum('yes','no') NOT NULL DEFAULT 'no',
+  PRIMARY KEY (`id`),
+  KEY `parent_section_id` (`parent_section_id`,`child_section_id`,`child_section_field_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- *** DATA: `sym_sections_association` ***
